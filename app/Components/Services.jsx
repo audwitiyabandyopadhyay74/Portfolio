921170f330936fbd2abf1f5079497965d8d5fd59
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2025-01-26 21:38:24 +0530

    Har Har Mahadev

--- app/Components/Services.jsx
+++ app/Components/Services.jsx
@@ -4,7 +4,7 @@ import { ServiceData } from '../Data/Service.js';
 
 const Services = () => {
   return (
-    <div className="lg: relative w-screen h-auto lg:h-auto mt-[150vh] lg:mt-[100vh] overflow-hidden gap-[100px]" style={{display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"center"}}>
+    <div className="lg: relative h-auto lg:h-auto mt-[150vh] lg:mt-[100vh] overflow-hidden gap-[100px]" style={{width:"100%",display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"center"}}>
       <span className='text-4xl md:text-8xl font-bold uppercase'>Services</span>
       <div className="flex w-screen h-max items-center justify-evenly flex-col lg:flex-row flex-wrap gap-4">
         {
