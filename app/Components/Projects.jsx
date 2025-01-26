93a8f9c6a16e4f84a2ca7baa62d45736d9753f69
Your Name <102481717+audwitiyabandyopadhyay74@users.noreply.github.com>
2025-01-26 15:09:01 +0530

    Har Har Mahadev

--- app/Components/Projects.jsx
+++ app/Components/Projects.jsx
@@ -5,7 +5,7 @@ import { ProjectData } from "../Data/Projects";
 const Projects = () => {
   return (
     <div 
-    className= "lg: relative w-screen h-[130vh] lg:h-auto mt-[150vh] lg:mt-[100vh] overflow-hidden flex items-center gap-[100px] justify-center"
+    className="lg: relative w-screen h-auto lg:h-auto mt-[150vh] lg:mt-[100vh] overflow-hidden flex flex-col items-center gap-[100px] justify-center"
     >
       <h1 className="w-[80%] text-center text-4xl md:text-8xl font-bold uppercase">Projects</h1>
       <div className="w-screen h-auto flex justify-center items-center flex-wrap gap-4 md:gap-10 mt-4">
--- app/Components/Services.jsx
+++ app/Components/Services.jsx
@@ -4,7 +4,7 @@ import { ServiceData } from '../Data/Service.js';
 
 const Services = () => {
   return (
-    <div className=" lg: relative w-screen h-[130vh] lg:h-auto mt-[150vh] lg:mt-[100vh] overflow-hidden flex items-center gap-[100px] justify-center  ">
+    <div className="lg: relative w-screen h-auto lg:h-auto mt-[150vh] lg:mt-[100vh] overflow-hidden flex flex-col items-center gap-[100px] justify-center">
       <span className='text-4xl md:text-8xl font-bold uppercase'>Services</span>
       <div className="flex w-screen h-max items-center justify-evenly flex-col lg:flex-row flex-wrap gap-4">
         {
